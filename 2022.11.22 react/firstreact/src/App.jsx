85b60b54d0e8b50a269b025d3e13fe6457a127ce
Your Name <you@example.com>
2022-12-05 10:14:37 +0100

    12/05/2022

--- 2022.11.22 react/firstreact/src/App.jsx
+++ 2022.11.22 react/firstreact/src/App.jsx
@@ -1,21 +1,40 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
 import { Counter } from "./components/Counter";
+import Home from "./components/Home";
 import Login from "./components/Login";
 import { Todo } from "./components/Todo";
+import { MyNavBar } from "./components/MyNavBar";
 import { useState } from "react";
-
+import { Products } from "./components/Products";
+import { Product } from "./components/Product";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="container p-5">
-      <h1 className="border-bottom border-info text-center">
-        My First React App
-      </h1>
-      {/* <Counter /> */}
+      <MyNavBar />
+      <div className="holder">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="counter" element={<Counter />} />
+          <Route
+            path="login"
+            element={<Login setIsLoggedIn={setIsLoggedIn} />}
+          />
+          <Route
+            path="todo" 
+            element={<Todo setIsLoggedIn={setIsLoggedIn} />} 
+          />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<Product />} />
+        </Routes>
+      </div>
+      {/*
       {!isLoggedIn && <Login setIsLoggedIn={setIsLoggedIn} />}
       {isLoggedIn && <Todo setIsLoggedIn={setIsLoggedIn} />}
+      */}
     </div>
   );
 }
